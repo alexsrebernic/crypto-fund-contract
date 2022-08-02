ce9d8857ad9151387e82e9fe9cd0ff4c719c0c35
alexsreberni <alexsrebernic@gmail.com>
2022-08-01 21:26:36 -0300

    add aggregator and donations contract initialization, add function to get current price of eth and conversion rate

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.9;
 import "./Donee.sol";
 contract Donations {
     address payable public owner;
-    address[] public donees;
-    mapping (address => address) userToContract;
-    event newDonee (address _donee);
+    userAddressAndContractdAdress[] public donees;
+    struct userAddressAndContractdAdress {
+        address user_address;
+        address contract_address;
+    }
+    event newDonee (address _donee_contract);
     modifier onlyOwner() {
         if (msg.sender != owner) revert FundMe__NotOwner();
         _;
@@ -13,17 +16,14 @@ contract Donations {
     constructor() {
         owner = payable(msg.sender);
     }
-    function pushDonee(address _donee) external {
-        donees.push(_donee);
-        emit newDonee(_donee);
+    function pushDonee(address _user,address _doneeContract) external {
+        donees.push(userAddressAndContractdAdress(_user, _doneeContract));
+        emit newDonee(_doneeContract);
     }
     function killContract() external onlyOwner {
         selfdestruct(owner);
     }
-    function getUserDoneeContract(address _user) external view returns(address){
-        return userToContract[_user];
-    }
-    function getDonees() public view onlyOwner returns(address[] memory) {
+    function getDonees() public view onlyOwner returns(userAddressAndContractdAdress[] memory) {
         return donees;
     }
 }
--- contracts/Donee.sol
+++ contracts/Donee.sol
@@ -5,16 +5,17 @@ error InsufficientBalance(uint256 required);
 error FundMe__NotOwner();
 
 import "./Donations.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract Donee {
     address private immutable i_owner;
-    uint public constant MINIMUN_USD = 0.01 ether;
+    uint public constant MINIMUN_USD = 1 * 1e18;
     address[] private s_donors;
     mapping(address => uint256) private s_addressToAmountDonated;
-    address public constant DONATIONS_CONTRACT = 0x8016619281F888d011c84d2E2a5348d9417c775B;
-    DonationsInterface DonationsContract = DonationsInterface(DONATIONS_CONTRACT);
+    DonationsInterface private immutable donationsContract;
+    AggregatorV3Interface private immutable aggregatorV3Contract;
     DataDonee public  dataDonee;
 
-    event donation (address indexed _from, address indexed _to, uint amount);
+    event donation (address indexed _from,  uint amount);
     struct DataDonee {
         address owner;
         uint  balance;
@@ -29,30 +30,32 @@ contract Donee {
         if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
-    constructor(string memory _first_name, string memory _last_name, string memory _avatar_color,string memory _details, string memory _created_at){
+    constructor(string memory _firstName, string memory _lastName, string memory _avatarColor,string memory _details, string memory _createdAt,address _aggregatorV3Interface, address _donationsInterface){
         i_owner = msg.sender;
-        dataDonee = DataDonee(
+        donationsContract = DonationsInterface( _donationsInterface );
+        aggregatorV3Contract = AggregatorV3Interface( _aggregatorV3Interface );
+        dataDonee = DataDonee (
             msg.sender,
             0,
-            _first_name,
-            _last_name,
-            _avatar_color,
+            _firstName,
+            _lastName,
+            _avatarColor,
             _details,
-            _created_at,
+            _createdAt,
             address(this)
         );
-        DonationsContract.pushDonee(address(this));
+        donationsContract.pushDonee( msg.sender, address(this) );
     }
     function donate() public payable {
-        if(msg.value < MINIMUN_USD){ 
-            revert InsufficientBalance({
-            required: MINIMUN_USD
+        if(getConversionRate(msg.value) < MINIMUN_USD){ 
+                revert InsufficientBalance({
+                required: MINIMUN_USD
             });
         }
         s_addressToAmountDonated[msg.sender] += msg.value;
         s_donors.push(msg.sender);
         dataDonee.balance += msg.value;
-        emit donation(msg.sender,dataDonee.owner,msg.value);
+        emit donation( msg.sender,  msg.value );
     }
     function withdraw() public onlyOwner {
         address[] memory donors = s_donors;
@@ -68,7 +71,16 @@ contract Donee {
         (bool success, ) = i_owner.call{value: address(this).balance}("");
         require(success);
     }
-    function getAddressAmountDonated(address _donor) internal view returns (uint256) {
+    function getPrice() internal view returns(uint256) {
+        (, int256 price,,,) = aggregatorV3Contract.latestRoundData();
+        return uint(price * 1e10);
+    }
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+    function getAddressAmountDonated( address _donor ) internal view returns (uint256) {
         return s_addressToAmountDonated[_donor];
     }
     function getDataDonee() external view returns (DataDonee memory) {
@@ -76,5 +88,5 @@ contract Donee {
     }
 }
 interface DonationsInterface {
-    function pushDonee(address) external ;
+    function pushDonee( address, address ) external ;
 }
